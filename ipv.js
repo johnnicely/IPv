349b2e7c0185efb5481cdec39e88fda8cb02040e
John Nicely <john.nicely@gmail.com>
2016-04-22 15:53:30 -0700

    Move license to separate file.

--- ipv.js
+++ ipv.js
@@ -1,34 +1,15 @@
-/*
-
-The software below is based on Beau Gunderson's IPv4 / IPv6 library,
-originally available at:
-
-https://github.com/beaugunderson/javascript-ipv6
-
-In accordance with the license provided with his software, this library is also
-licensed under the same terms. The license is provided below.
-
-Copyright (C) 2011 by Beau Gunderson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-*/
+/**
+ * 
+ * The software below is based on Beau Gunderson's IPv4 / IPv6 library,
+ * originally available at:
+ * 
+ * https://github.com/beaugunderson/javascript-ipv6
+ * 
+ * In accordance with the license provided with his software, this library is also
+ * licensed under the same terms (MIT License). The license is provided in the LICENSE
+ * file included with this repository.
+ *
+ **/
 
 // 0 => full match
 // 1 => full IPv4 address
@@ -42,10 +23,11 @@ RE_VALID_IPV4 = /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2
 // 4 => zone, if present
 RE_VALID_IPV6 = /^((?:(?=(?:.*?::.*?|(?:[^:]*?:){7}))(?!(?:[^:]+:){8,})(?:(?:::ffff:((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))|(?:(?:::(?!.*::))?(?:[0-9a-f]{1,4}(?:(?:::(?!.*::))|:(?=[0-9a-f])|:(?!(?:$|\/))|$|(?=(?:\/|%)))){0,8}))))(?:\/(1[0-1][0-9]|12[0-8]|[0-9]{1,2}))?(?:%(.*))?$/i;
 
-/* Simple JavaScript Inheritance
+/**
+ * Simple JavaScript Inheritance
  * By John Resig http://ejohn.org/
- * MIT Licensed.
- */
+ * Also licensed under MIT License.
+ **/
 (function()
 {
 	var initializing = false,
